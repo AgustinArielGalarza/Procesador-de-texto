3ab77e80e464956ef9cc512332d2e0befd05ae4d
Agus <aguusg015@gmail.com>
2019-03-12 19:52:35 -0300

    Menu optimizado

--- src/procesadordetexto/ProcesadorDeTexto.java
+++ src/procesadordetexto/ProcesadorDeTexto.java
@@ -1,37 +1,67 @@
 package procesadordetexto;
 
 import java.awt.BorderLayout;
+import javafx.scene.text.Font;
 import javax.swing.*;
 
 public class ProcesadorDeTexto {
 
     public static void main(String[] args) {
         Marco mimarco = new Marco();
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
-    
 }
+    
+
 class Marco extends JFrame{
      public Marco(){
-            setVisible(true);
             setTitle("Procesador de Texto");
             setBounds(250, 200, 600, 450);
             Lamina milam = new Lamina();
             add(milam);
-            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            
+            setVisible(true);
         }
 }
 class Lamina extends JPanel{
+    
+    JTextPane papel;
+    JMenu fuentes,estilos,tamano;
+    Font letras;
+    
     public Lamina (){
         setLayout(new BorderLayout());
         JPanel lamenu = new JPanel();
         JMenuBar barra = new JMenuBar();
-        JMenu fuentes = new JMenu("Fuentes");
-        JMenu estilos = new JMenu("Estilos");
-        JMenu tamano = new JMenu("Tamaño");
+        fuentes = new JMenu("Fuentes");
+        estilos = new JMenu("Estilos");
+        tamano = new JMenu("Tamaño");
+        //----------------------------------------------------------------------
+        Configura_menu("Arial", "Fuentes");
+      
         barra.add(fuentes);
         barra.add(estilos);
         barra.add(tamano);
         lamenu.add(barra);
+        
         add(lamenu,BorderLayout.NORTH);
+        papel = new JTextPane();
+        add(papel,BorderLayout.CENTER);
+        
     }
+    public void Configura_menu (String rotulo, String menu){
+        
+        JMenu elem_menu = new JMenu(rotulo);
+        
+        if(menu =="Fuentes"){
+            
+            fuentes.add(elem_menu);
+            
+        }else if (menu == "Estilos"){
+            estilos.add(elem_menu);
+        }else {
+            tamano.add(elem_menu);
+        }
+    }
+
 }
