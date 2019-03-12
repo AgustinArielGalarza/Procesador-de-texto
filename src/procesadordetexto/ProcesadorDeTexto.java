4f88faa804d6fd731417d4fc20e0a14cbcee7d9b
Agus <aguusg015@gmail.com>
2019-03-11 21:59:05 -0300

    Menu creado

--- src/procesadordetexto/ProcesadorDeTexto.java
+++ src/procesadordetexto/ProcesadorDeTexto.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package procesadordetexto;
 
-/**
- *
- * @author Agustin
- */
+import java.awt.BorderLayout;
+import javax.swing.*;
+
 public class ProcesadorDeTexto {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Marco mimarco = new Marco();
     }
     
+}
+class Marco extends JFrame{
+     public Marco(){
+            setVisible(true);
+            setTitle("Procesador de Texto");
+            setBounds(250, 200, 600, 450);
+            Lamina milam = new Lamina();
+            add(milam);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+        }
+}
+class Lamina extends JPanel{
+    public Lamina (){
+        setLayout(new BorderLayout());
+        JPanel lamenu = new JPanel();
+        JMenuBar barra = new JMenuBar();
+        JMenu fuentes = new JMenu("Fuentes");
+        JMenu estilos = new JMenu("Estilos");
+        JMenu tamano = new JMenu("Tamaño");
+        barra.add(fuentes);
+        barra.add(estilos);
+        barra.add(tamano);
+        lamenu.add(barra);
+        add(lamenu,BorderLayout.NORTH);
+    }
 }
